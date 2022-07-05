bea99a3916be286cbeb8add4f10473b4b81d662a
swarangisatpute263 <swarangisatpute263@example.com>
2022-07-05 15:42:38 +0530

    Initial steps, understanding & basic types

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Basic Types
+let id = 26;
+let person = 'Swarangi';
+let isWorking = true;
+let x = 'Hello';
+let age;
+age = 8;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+//Basic Types
+let id: number = 26
+let person: string = 'Swarangi'
+let isWorking: boolean = true
+let x:any = 'Hello'
+
+let age:number 
+age=8
+
