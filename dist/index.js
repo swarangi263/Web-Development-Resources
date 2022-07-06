dffb2fe508f3e4fcd6a3d990c9848c630ca90014
swarangisatpute263 <swarangisatpute263@example.com>
2022-07-06 14:06:51 +0530

    dynamic datatypes

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,35 @@ let isWorking = true;
 let x = 'Hello';
 let age;
 age = 8;
+//Arrays
+let seq = [1, 2, 3];
+let arr = [1, true, "swara"];
+//Tuple
+let people = [1, 'swara', true];
+//Tuple Array
+let employee;
+employee = [
+    [1, 'swara'],
+    [2, 'sinu'],
+    [3, 'asa']
+];
+//Unions
+let e_id;
+e_id = '22';
+//Enum - name constants 
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Right"] = 3] = "Right";
+    Direction1[Direction1["Left"] = 4] = "Left";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Right"] = "Right";
+    Direction2["Left"] = "Left";
+})(Direction2 || (Direction2 = {}));
+//Object
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,64 @@ let x:any = 'Hello'
 let age:number 
 age=8
 
+//Arrays
+let seq: number[] = [1,2,3]
+let arr: any[] = [1, true, "swara"]
+
+//Tuple
+let people: [number, string, boolean] = [1, 'swara', true]
+
+//Tuple Array
+let employee: [number, string] [] 
+employee = [
+    [1, 'swara'],
+    [2, 'sinu'],
+    [3, 'asa']
+]
+
+//Unions - holds more than 1 type
+let e_id: string | number 
+e_id = '22'
+
+//Enum - name constants 
+enum Direction1{
+    Up = 1,
+    Down,
+    Right,
+    Left
+}
+console.log(Direction1)
+
+enum Direction2{
+    Up = 'Up',
+    Down = 'Down',
+    Right = 'Right',
+    Left = 'Left'
+}
+
+//Objects- instance containing key value pair
+
+//type template
+type User = {
+    id:number
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'Swara'
+}
+
+
+//Void - no return type
+
+function greet() : void {
+    console.log("Hello")
+}
+
+//only null & undefined is assigned to void
+
+let a : void = undefined
+// let b: void = 1 //Error
+
+
